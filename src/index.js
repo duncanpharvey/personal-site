f9408f71ad7c80b2da1acb0e4861614202f973b1
Duncan Harvey <duncanpharvey@gmail.com>
2023-07-21 19:41:21 -0400

    adds datadog RUM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
+import { datadogRum } from '@datadog/browser-rum';
 import App from './App';
 
 // See https://reactjs.org/docs/strict-mode.html
@@ -18,3 +19,20 @@ if (rootElement.hasChildNodes()) {
   const root = createRoot(rootElement);
   root.render(<StrictApp />);
 }
+
+datadogRum.init({
+  applicationId: '4f7ca778-f45e-49e1-9069-51392cfc1d48',
+  clientToken: 'pube44ae88f4243611fc1a52b27e958738d',
+  site: 'datadoghq.com',
+  service: 'duncanpharvey.com',
+  env: 'prod',
+  version: '1.0.0',
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 20,
+  trackUserInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: 'mask-user-input',
+});
+
+datadogRum.startSessionReplayRecording();
